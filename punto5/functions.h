2db8f86c27c29651c34d0e3575bc69e448df16e7
maolok <42081510+maolok@users.noreply.github.com>
2018-10-11 07:57:19 -0500

    Add files via upload

---
+++ punto5/functions.h
@@ -0,0 +1,21 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+struct Tiempo{
+
+    int horas;
+    int minutos;
+    int segundos;
+
+
+};
+
+void cronometro(Tiempo *datos);
+
+
+#endif // FUNCTIONS_H
